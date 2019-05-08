03294672e3c8ec0572423ae21446e5ce829fe62f
in413hs <in413hs@gmail.com>
2019-05-08 16:35:16 +0900

    Add sum function

--- Calculator.java
+++ Calculator.java
@@ -3,6 +3,12 @@ package com.mycompamy.test;
 public clas Calculator{
 
     public static void main(String[] args){
-      
+      int a = 4;
+      int b = 2;
+      System.out.System.out.println(sum(a,b));
+    }
+
+    private static int sum(int a, int b){
+      return a+b;
     }
 }
